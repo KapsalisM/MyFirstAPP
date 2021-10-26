0db56ea491a2fcaff83c3a872cba0af293011ed7
KapsalisM <mkapsalismos@gmail.com>
2021-10-26 22:02:34 +0300

    my first commit

---
+++ koskino.py
@@ -0,0 +1,21 @@
+
+n=input("give number ")
+while n <1 or n >30000:
+    n=input("give number ")
+
+K=[]
+for i in range(2,n):
+    K.append(i)
+
+p=2
+while p**2<n:
+    for i in range(p-1,len(K)):
+        if K[i]%p==0:
+            K[i]=0
+    p += 1
+    while K[p-1] ==0:
+        p+=1
+
+for i in range(len(K)):
+    if K[i]!=0:
+        print K[i],"prwtoi arithmoi"
